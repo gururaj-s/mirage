1cdf68136662a41b489f8c3f68915e278de566fa
Gururaj Saileshwar <gsaileshwar3@gatech.edu>
2021-01-26 20:28:12 -0500

    Updated RunScript for BaseWays=8 experiments

---
+++ security_analysis/results/base/run_base.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+## Command Line Arguments
+NUM_BILLION_BALLS_PER_EXP=${1:-100} # $1
+NUM_EXP=${2:-1}               # $2
+NUM_BILLIONS=$(($NUM_BILLION_BALLS_PER_EXP * $NUM_EXP))
+
+echo "Running Buckets & Balls Simulation for WaysPerSkew=8"
+echo "(Throwing ${NUM_BILLIONS} Billion Balls)"
+mkdir -p raw_results;
+rm -rf raw_results/*;
+
+## Run Empirical Results for Extra-Ways = 1,2,3,4,5,6,7,8
+for extra_ways_per_skew in 1 2 3 4 5 6 7 8
+do
+    ## Run $NUM_EXP experiments
+    for (( i=0; i<$NUM_EXP; i++ ))
+    do
+        stdbuf -oL ../../bin/mirage8WPS_kExtraWays_NBn.o $extra_ways_per_skew $NUM_BILLION_BALLS_PER_EXP $i \
+               > raw_results/Base8.ExtraWays${extra_ways_per_skew}.Exp${i}.out &
+    done
+done
+
+## Wait for completion of all experiments.
+exp_count=`ps aux | grep -i "mirage8WPS_kExtraWays_NBn.o" | grep -v "grep" | wc -l`
+while [ $exp_count -gt 0 ]
+do
+    sleep 30
+    exp_count=`ps aux | grep -i "mirage8WPS_kExtraWays_NBn.o" | grep -v "grep" | wc -l`    
+done
