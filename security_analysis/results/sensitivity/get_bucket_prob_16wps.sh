ced5b6e958f50fcb5baa6c285c7acf2cfe55718a
Gururaj Saileshwar <gsaileshwar3@gatech.edu>
2021-01-27 17:29:57 -0500

    Added Statistics Scripts for Sensitivity experiments in Security Analysis

---
+++ security_analysis/results/sensitivity/get_bucket_prob_16wps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+## Temp Files
+rm -f bucket_occ_temp;
+touch bucket_occ_temp;
+
+## Get Total number of experiments
+tot_exp=`ls raw_results/Base16.ExtraWays6.out | wc -l`;
+
+## Get Line Number for Distribution of Bucket-Occupancy
+line_num=`grep -n "Distribution of Bucket-Occupancy (Averaged across Ball Throws) => Used for P(Bucket = k balls) calculation" raw_results/Base16.ExtraWays6.out | awk -F':' '{print $1}'`
+start_line_num=$(($line_num+3))
+
+## Get the Bucket-Occupancy for an experiment & add it to bucket_occ_temp counts
+tail -n+$start_line_num raw_results/Base16.ExtraWays6.out | head -n33 | awk -F':' '{print $2}' | awk '{print $1}' >bucket_occ_curr
+paste bucket_occ_temp bucket_occ_curr > bucket_occ_next;
+awk '{print $1+$2}' bucket_occ_next > bucket_occ_temp
+
+## Total-Ball-Count (2x Ball-thrown, as statistic counts one ball twice)
+total_ball_count=`awk '{sum+=$1}END{print sum}' bucket_occ_temp`;
+
+## Output Bucket-Probabilities
+echo "BallsInBucket-N Pr_obs(N)" > Base16.BucketProb.stat;
+awk -v count=$total_ball_count '{print NR-1, $1/count}' bucket_occ_temp >> Base16.BucketProb.stat
+column -t Base16.BucketProb.stat > temp
+mv temp Base16.BucketProb.stat
+
+## Remove Temp Files
+rm -f bucket_occ_curr bucket_occ_next bucket_occ_temp;
--- security_analysis/results/sensitivity/run_sensitivity.sh
+++ security_analysis/results/sensitivity/run_sensitivity.sh
@@ -3,12 +3,14 @@ echo "Running Sensitivity: Buckets & Balls Simulation for WaysPerSkew=4,16"
 echo "(Throwing 1 Billion Balls)"
 mkdir -p raw_results;
 rm -rf raw_results/*;
+rm -rf Base*.BucketProb.stat;
+
 #LLC-Associativity=32
-stdbuf -oL ../../bin/mirage16WPS_kExtraWays_NBn.o 5 1 42 \
-       > raw_results/Base16.ExtraWays5.out &
+stdbuf -oL ../../bin/mirage16WPS_kExtraWays_NBn.o 6 1 42 \
+       > raw_results/Base16.ExtraWays6.out &
 #LLC-Associativity=8
-stdbuf -oL ../../bin/mirage4WPS_kExtraWays_NBn.o 5 1 42 \
-       > raw_results/Base4.ExtraWays5.out &
+stdbuf -oL ../../bin/mirage4WPS_kExtraWays_NBn.o 6 1 42 \
+       > raw_results/Base4.ExtraWays6.out &
 
 ## Wait for completion of all experiments.
 exp_count=`ps aux | grep -i "mirage" | grep -v "grep" | wc -l`
@@ -19,5 +21,5 @@ do
 done
 
 ##Generate Bucket_Probabilities
-## TODO: ./get_bucket_prob_16.sh
-## TODO: ./get_bucket_prob_4.sh
+./get_bucket_prob_16wps.sh
+./get_bucket_prob_4wps.sh
