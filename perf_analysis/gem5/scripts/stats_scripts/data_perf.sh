f16299df9dde83981c8d6d5972dfc2dd43bc4107
Gururaj Saileshwar <gsaileshwar3@gatech.edu>
2021-01-27 05:52:50 -0500

    Updated results scripts

--- perf_analysis/gem5/scripts/stats_scripts/data_perf.sh
+++ perf_analysis/gem5/scripts/stats_scripts/data_perf.sh
@@ -1,3 +1,3 @@
 cd ..
-
-perl getdata.pl -w spec2006_hmpki_noomnet -gmean -n 0   -nstat system.switch_cpus.numCycles -dstat sim_inst   -d  ../output/ooo_4Gmem_10Bn/Old_Ckpt_Test_2MB_16Way/BaselineRRIP   ../output/ooo_4Gmem_10Bn/Old_Ckpt_Test_2MB_16Way/scatter-cache/  ../output/ooo_4Gmem_10Bn/16WayLLC_SKEWFAIR_tdr1_75/skew-vway-rand   ../output/ooo_4Gmem_10Bn/16WayLLC_SKEWFAIR_tdr1_75/skew-vway-rand-datareuserepl > stats_scripts/data/perf.stat
+0;95;0c
+perl getdata.pl -w spec2006_hmpki_noomnet -gmean -n 0   -nstat system.switch_cpus.numCycles -dstat sim_inst   -d  ../output/ooo_4Gmem_10Bn/Old_Ckpt_Test_2MB_16Way/BaselineRRIP   ../output/ooo_4Gmem_10Bn/Old_Ckpt_Test_2MB_16Way/scatter-cache/  ../output/ooo_4Gmem_10Bn/16WayLLC_SKEWFAIR_tdr1_75/skew-vway-rand   > stats_scripts/data/perf.stat
